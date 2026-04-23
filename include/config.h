ae46ffc268103b0f3e817c923b7fdc0500585b6c
Julian V <j.vilfort01@gmail.com>
2026-04-23 18:54:19 -0400

    Update CAN IDs

--- include/config.h
+++ include/config.h
@@ -1,4 +1,4 @@
-#define TALON_L_CAN_ID 1
-#define TALON_R_CAN_ID 2
-#define VICTOR_L_CAN_ID 3
-#define VICTOR_R_CAN_ID 4
+#define TALON_L_CAN_ID 2
+#define TALON_R_CAN_ID 0
+#define VICTOR_L_CAN_ID 1
+#define VICTOR_R_CAN_ID 0
